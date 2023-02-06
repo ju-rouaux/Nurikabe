09000674ca2522b69f022b6a0dd912c881eeb5f6
Julien Rouaux <ju.rouaux@gmail.com>
2023-02-06 16:34:05 +0100

    Méthode stop au main

--- src/main/java/com/l3infogrp5/nurikabe/Main.java
+++ src/main/java/com/l3infogrp5/nurikabe/Main.java
@@ -6,16 +6,26 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    
 
     @Override
     public void start(Stage stage) throws Exception {
         ControllerMenuPrincipal menu = new ControllerMenuPrincipal(stage);
-        
-        //TODO : ajouter une fermeture du programme propre qui s'assure de la sauvegarde de la partie.
+
 
         stage.setScene(menu.getScene());
         stage.show();
     }
+
+
+    /*
+     * UTILISEZ Platform.exit() AU LIEU DE System.exit() POUR QUE CETTE METHODE SOIT TOUJOURS APPELÉE
+     */
+    @Override
+    public void stop() throws Exception {
+        //TODO : ajouter une fermeture du programme propre qui s'assure de la sauvegarde de la partie.
+    }
+    
 
     public static void main(String[] args) {
         launch(args);
