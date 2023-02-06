8bd3b0ac9fe0bfe0eaf3063826176b50be91665e
Nicolas.G_ <nicolas.gouget.etu@univ-lemans.fr>
2023-02-06 11:01:47 +0100

    Refactorisation des fichier menu selonnorme de dev

---
+++ src/main/java/com/l3infogrp5/nurikabe/menus/BouttonRetour.java
@@ -0,0 +1,25 @@
+/**
+ * Classe implémentant le bouton retour
+ * 
+ * @author 
+ */
+
+
+package com.l3infogrp5.nurikabe.menus;
+
+public class BouttonRetour {
+
+    BouttonRetour(){
+        
+    }
+
+
+    /**
+     * Méthode qui permet de revenir a l'ancienne affichage
+     * 
+     * 
+     */
+    public void chargeRetour() {
+
+    }
+}
