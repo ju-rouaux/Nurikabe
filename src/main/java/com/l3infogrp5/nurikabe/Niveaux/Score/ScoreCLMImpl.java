73342ad3d8c20f08f07d8099a89decbc99c073bd
Couapel Antoine <s192357@ic2s120-04.univ-lemans.fr>
2023-02-06 10:30:32 +0100

    Ajout niveaux

---
+++ src/main/java/com/l3infogrp5/nurikabe/Niveaux/Score/ScoreCLMImpl.java
@@ -0,0 +1,10 @@
+import java.util.Timer;
+
+public class ScoreCLMImpl extends ScoreChronoImpl {
+
+    public ScoreCLMImpl(Timer tps) {
+        super(tps);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
