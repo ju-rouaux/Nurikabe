f21e1d98583f7cbcdb1fa9e682b87fb170af9edf
Julien Rouaux <ju.rouaux@gmail.com>
2023-02-15 16:25:05 +0100

    Ajout Position.toString() + déplacement dossier utils

---
+++ src/main/java/com/l3infogrp5/nurikabe/utils/Position.java
@@ -0,0 +1,54 @@
+package com.l3infogrp5.utils;
+
+/**
+ * Structure représentant des coordonnées.
+ * 
+ * @author Julien Rouaux
+ */
+public class Position {
+    private int x, y;
+    private int index;
+
+    /**
+     * Créer de nouvelles coordonnées
+     * @param x position en x.
+     * @param y position en y.
+     * @param index index dans la matrice (x*nb_colonne + y).
+     */
+    public Position(int x, int y, int index) {
+        this.x = x;
+        this.y = y;
+        this.index = index;
+    }
+
+    /**
+     * Retourne la position en X.
+     * @return la position en X.
+     */
+    public int getX() {
+        return this.x;
+    }
+
+    /**
+     * Retourne la position en Y.
+     * @return la position en Y.
+     */
+    public int getY() {
+        return this.y;
+    }
+
+    /**
+     * Retourne l'index.
+     * @return l'index.
+     */
+    public int getIndex() {
+        return this.index;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public String toString(){
+        return "[Position ("+this.x+", "+this.y+") / Index "+this.index+"]";
+    }
+}
