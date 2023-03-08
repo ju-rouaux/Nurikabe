a6a706f046b5b947cb147c2f8029b74d1e5ef3e9
“Mael <mael.vassenet@gmail.com>
2023-03-08 12:00:34 +0100

    Probleme de réduction du nombre d'étoiles du score

--- src/main/java/com/l3infogrp5/nurikabe/Nurikabe.java
+++ src/main/java/com/l3infogrp5/nurikabe/Nurikabe.java
@@ -1,58 +1,43 @@
 package com.l3infogrp5.nurikabe;
 
-import java.io.IOException;
-
-import com.l3infogrp5.nurikabe.niveau.score.ScoreZen;
+import com.l3infogrp5.nurikabe.niveau.score.ScoreCLM;
+import com.l3infogrp5.nurikabe.niveau.score.ScoreChrono;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import org.controlsfx.control.Rating;
 
 public class Nurikabe extends Application {
-
-    double etoiles = 5.0;
 
     @Override
     public void start(Stage primaryStage) {
 
-        FXMLLoader loader;
-        Rating score;
-        
-        Pane root = new Pane();
-        
-        loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource("/FXML/score_etoile.fxml"));
-        loader.setController(this);
-        try {
-            root = loader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        final Text text = new Text("00:00");
+        text.setLayoutX(100);
+        text.setLayoutY(100);
+        text.setFill(Color.BLUE);
+        final Pane root = new Pane();
+        root.getChildren().setAll(text);
+        final Scene scene = new Scene(root, 350, 300);
 
-        final ScoreZen score = new ScoreZen(etoiles);
+        ScoreChrono score = new ScoreCLM(10, 0, text);
+        score.calcul();
 
-        Label label = new Label("Your rating: " + etoiles);
-        label.setTextFill(Color.BLACK);
+        System.out.println("test");
+        score.aideUtilise();
 
-        HBox hBox = new HBox(score, label);
-        hBox.setSpacing(10);
-        hBox.setAlignment(Pos.CENTER);
+        /*
+         * ScoreChrono score = new ScoreEndless(40,0, text);
+         * score.calcul();
+         * 
+         * System.out.println("test");
+         * //score.aideUtilise(35);
+         * score.grilleComplete();
+         */
 
-        VBox vBox = new VBox(hBox);
-        vBox.setAlignment(Pos.CENTER);
-
-        Scene scene = new Scene(vBox, 300, 200);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-        primaryStage.setTitle("Test score etoile");
+        primaryStage.setTitle("Test sur l'opacité");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
