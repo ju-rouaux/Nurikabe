f194cadc3d12bc62a8b8282f2451351266e9d56a
Nicolas.G_ <nicolas.gouget.etu@univ-lemans.fr>
2023-03-15 21:40:22 +0100

    suppression import inutile

--- src/main/java/com/l3infogrp5/nurikabe/menu/ControllerMenuModeJeu.java
+++ src/main/java/com/l3infogrp5/nurikabe/menu/ControllerMenuModeJeu.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 
 import java.io.IOException;
