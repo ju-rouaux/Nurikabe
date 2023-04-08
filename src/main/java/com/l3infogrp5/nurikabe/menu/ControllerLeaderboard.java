7ca76938d5d5f460ffc12165b56f7d64a61265fe
Guillaume Richard <guillaume.rwins@gmail.com>
2023-04-08 12:12:30 +0200

    Commencement donnees leaderboard

--- src/main/java/com/l3infogrp5/nurikabe/menu/ControllerLeaderboard.java
+++ src/main/java/com/l3infogrp5/nurikabe/menu/ControllerLeaderboard.java
@@ -1,19 +1,25 @@
 package com.l3infogrp5.nurikabe.menu;
 
-import javafx.scene.control.cell.*;
+import com.l3infogrp5.nurikabe.profil.Profil;
+import com.l3infogrp5.nurikabe.sauvegarde.Sauvegarder;
+import com.l3infogrp5.nurikabe.utils.Path;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.collections.*;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.Date;
+import java.util.List;
 
 /**
  * Contrôleur du Leaderboard
@@ -22,178 +28,131 @@ import java.sql.Date;
  */
 
 public class ControllerLeaderboard {
-private Stage stage;
-private FXMLLoader loader;
-private Scene scene;
-private int id_niveau;
+    private final Stage stage;
+    private final FXMLLoader loader;
+    private final Scene scene;
+    private final int id_niveau;
 
-@FXML
-private BorderPane borderPane;
+    @FXML
+    private BorderPane borderPane;
 
-@FXML
-private Button btn_retour;
+    @FXML
+    private Button btn_retour;
 
-@FXML
-private TableColumn<Scoring, Date> date;
+    @FXML
+    private TableColumn<Sauvegarder.DonneesScore, Date> date;
 
-@FXML
-private TableColumn<Scoring, Date> date_moi;
+    @FXML
+    private TableColumn<Sauvegarder.DonneesScore, Date> date_moi;
 
-@FXML
-private TableColumn<Scoring, String> nom;
+    @FXML
+    private TableColumn<Sauvegarder.DonneesScore, String> nom;
 
-@FXML
-private TableColumn<Scoring, Double> score;
+    @FXML
+    private TableColumn<Sauvegarder.DonneesScore, Double> score;
 
-@FXML
-private TableColumn<Scoring, Double> score_moi;
+    @FXML
+    private TableColumn<Sauvegarder.DonneesScore, Double> score_moi;
 
-@FXML
-private TableView<Scoring> tableau;
+    @FXML
+    private TableView<Sauvegarder.DonneesScore> tableau;
 
-@FXML
-private TableView<Scoring> tableau_moi;
+    @FXML
+    private TableView<Sauvegarder.DonneesScore> tableau_moi;
 
 
-   /**
+    /**
      * Initialise le menu de sélection de mode de jeu et son contrôleur.
      *
-     * @param stage la fenêtre contenant la scène.
+     * @param stage     la fenêtre contenant la scène.
      * @param id_niveau le numéro du niveau.
      * @throws IOException lancé lorsque le fichier FXML correspondant n'a pas pû
      *                     être lu.
      */
-public ControllerLeaderboard(Stage stage,int id_niveau) throws IOException {
-    this.stage = stage;
-    this.id_niveau=id_niveau;
+    public ControllerLeaderboard(Stage stage, int id_niveau) throws IOException {
+        this.stage = stage;
+        this.id_niveau = id_niveau;
 
-    loader = new FXMLLoader();
-    loader.setLocation(getClass().getResource("/FXML/leaderboard.fxml"));
-    loader.setController(this);
+        loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/FXML/leaderboard.fxml"));
+        loader.setController(this);
 
-    scene = loader.load();
-}
+        scene = loader.load();
+    }
 
 
     /**
      * Charge l'image et le nom du niveau sur la carte.
-    */
-    public void initialize() { 
+     */
+    @FXML
+    public void initialize() {
 
 
-        this.date.setCellValueFactory(new PropertyValueFactory<>("Date"));
-        this.nom.setCellValueFactory(new PropertyValueFactory<>("nom"));
+        this.date.setCellValueFactory(new PropertyValueFactory<>("date"));
+        this.nom.setCellValueFactory(new PropertyValueFactory<>("nom_joueur"));
         this.score.setCellValueFactory(new PropertyValueFactory<>("score"));
+        List<String> pseudos = Sauvegarder.listeFichiers(new File(Path.repertoire_lvl.toString()));
+
 
         // ajout des valeurs
-        ObservableList<Scoring> items = FXCollections.observableArrayList(
-            new Scoring("é",22.1, Date.valueOf("2022-02-01")),
-            new Scoring("ze",13.0, Date.valueOf("2022-03-01"))
+        ObservableList<Sauvegarder.DonneesScore> items = FXCollections.observableArrayList(
+            new Sauvegarder.DonneesScore()
         );
+        for (String pseudo : pseudos) {
+            try {
+                List<Sauvegarder.DonneesScore> scores = Sauvegarder.chargerScore(pseudo, Profil.getModeDeJeu(), id_niveau, false);
+                if (scores != null) {
+                    for(Sauvegarder.DonneesScore score : scores) {
+                        System.out.println(score.getNomJoueur() + " " + score.getScore() + " " + score.getDate());
+                        items = FXCollections.observableArrayList(scores);
+                    }
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         this.tableau.setItems(items);
 
         // rendre les colonnes triables
         this.tableau.getSortOrder().addAll(date, nom, score);
-        
-
 
 
         this.date_moi.setCellValueFactory(new PropertyValueFactory<>("Date"));
-        this.score_moi.setCellValueFactory(new PropertyValueFactory<>("score"));
+        this.score_moi.setCellValueFactory(new PropertyValueFactory<>("Score"));
 
-        ObservableList<Scoring> items_moi = FXCollections.observableArrayList(
-            new Scoring("é",22.1, Date.valueOf("2022-02-01")),
-            new Scoring("ze",13.0, Date.valueOf("2022-03-01"))
-        );
+        ObservableList<Sauvegarder.DonneesScore> items_moi = null;
+
+        for (String pseudo : pseudos) {
+            try {
+                if (pseudo.equals(Profil.getJoueur())) {
+                    List<Sauvegarder.DonneesScore> scores = Sauvegarder.chargerScore(pseudo, Profil.getModeDeJeu(), id_niveau, true);
+                    items_moi = FXCollections.observableArrayList(scores);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
 
         this.tableau_moi.setItems(items_moi);
     }
 
-/*
- * Retourne au menu précédent, le menu de selection  .
- */
-@FXML
-private void retourClique(ActionEvent event) throws Exception {
-    stage.setScene(new ControllerMenuSelection(stage).getScene());
-}
-
-/**
- * Retourne la scène gérée par le contrôleur.
- * 
- * @return la scène gérée par le contrôleur.
- */
-public Scene getScene() {
-    return scene;
-}
-    /**
-     * Classe interne au leaderboard 
-     * 
-     * @author Cyprien PENNACHI
+    /*
+     * Retourne au menu précédent, le menu de selection  .
      */
+    @FXML
+    private void retourClique(ActionEvent event) throws Exception {
+        stage.setScene(new ControllerMenuSelection(stage).getScene());
+    }
 
-    public class Scoring{
-
-        private String nom;
-        private double score;
-        private Date date;
-        /**
-         * Initialise le menu de sélection de mode de jeu et son contrôleur.
-         *
-         * @param nom le nom de la personne.
-         * @param score le score de la personne.
-         * @param date la date du score fait. 
-         */
-        public Scoring(String nom, double score, Date date){
-            this.nom=nom;
-            this.score=score;
-            this.date=date;
-        }
-        /**
-         * Retourne le nom du profil.
-         * 
-         * @return le nom du profil.
-         */
-        public String getNom(){
-            return this.nom;
-        }
-                /**
-         * Retourne le nom du profil.
-         * 
-         * @return le score du profil.
-         */
-        public double getScore(){
-            return this.score;
-        }
-        /**
-         * Retourne la date du score rélisé.
-         * 
-         * @return le date.
-         */
-        public Date getDate(){
-            return this.date;
-        }
-        /**
-         * Change le nom du profil.
-         * @param nom le nom du profil.
-         */
-        public void setNom(String nom){
-            this.nom=nom;
-        }
-        /**
-         * change le score d'un profil.
-         * @param score le score du profil.
-         */
-        public void setScore(double score){
-            this.score=score;
-        }
-        /**
-         * Change la date du score réalisé.
-         * @param date la date du score réalisé.
-         */
-        public void setDate(Date date){
-            this.date=date;
-        }
+    /**
+     * Retourne la scène gérée par le contrôleur.
+     *
+     * @return la scène gérée par le contrôleur.
+     */
+    public Scene getScene() {
+        return scene;
     }
 }
 
--- src/main/java/com/l3infogrp5/nurikabe/sauvegarde/Sauvegarder.java
+++ src/main/java/com/l3infogrp5/nurikabe/sauvegarde/Sauvegarder.java
@@ -263,42 +263,46 @@ public class Sauvegarder {
      * @throws IOException {@link IOException} si le fichier n'existe pas
      */
     public static List<DonneesScore> chargerScore(String joueur, ModeDeJeu mode_de_jeu, int id_niveau, boolean niveau_en_cours) throws IOException {
-        System.out.println("Chargement des scores pour le joueur " + joueur + " en mode " + mode_de_jeu + "niveau_en_cours" + niveau_en_cours);
-        List<DonneesScore> scores = new ArrayList<>();
-        DonneesScore donneeScore = new DonneesScore("");
-        Reader file_reader;
-        if (!mode_de_jeu.equals(ModeDeJeu.DETENTE)) {
-            file_reader = new FileReader(Path.repertoire_score + "/" + mode_de_jeu + ".save");
-        } else {
-            file_reader = new FileReader(Path.repertoire_score + "/" + mode_de_jeu + "_" + id_niveau + ".save");
-        }
-        BufferedReader bufferedReader = new BufferedReader(file_reader);
-
-        while (bufferedReader.ready()) {
-            String line = bufferedReader.readLine();
-            String[] parts = line.split("%");
-            String nom_joueur = parts[0].trim();
-            if (!nom_joueur.equals(joueur)) continue;
-            String score = parts[1].trim();
-            String date = parts[2].trim();
-            String enCours = "";
-            if (mode_de_jeu != ModeDeJeu.SANSFIN)
-                enCours = parts[3].trim();
-            else {
-                enCours = "false";
+        if(RechercherSauvegardeNiveau(joueur, mode_de_jeu, id_niveau)) {
+            System.out.println("Chargement des scores pour le joueur " + joueur + " en mode " + mode_de_jeu + "niveau_en_cours" + niveau_en_cours);
+            List<DonneesScore> scores = new ArrayList<>();
+            DonneesScore donneeScore = new DonneesScore();
+            Reader file_reader;
+            if (!mode_de_jeu.equals(ModeDeJeu.DETENTE)) {
+                file_reader = new FileReader(Path.repertoire_score + "/" + mode_de_jeu + ".save");
+            } else {
+                file_reader = new FileReader(Path.repertoire_score + "/" + mode_de_jeu + "_" + id_niveau + ".save");
             }
-            if (!Boolean.parseBoolean(enCours) == niveau_en_cours) continue;
-            System.out.println("score : " + score + " date : " + date + " en cours : " + enCours + " mode de jeu : " + mode_de_jeu);
-            donneeScore.score = score;
-            donneeScore.date = date;
-            donneeScore.niveau_en_cours = enCours;
-            scores.add(donneeScore);
+            BufferedReader bufferedReader = new BufferedReader(file_reader);
+
+            while (bufferedReader.ready()) {
+                String line = bufferedReader.readLine();
+                String[] parts = line.split("%");
+                String nom_joueur = parts[0].trim();
+                if (!nom_joueur.equals(joueur)) continue;
+                String score = parts[1].trim();
+                String date = parts[2].trim();
+                String enCours = "";
+                if (mode_de_jeu != ModeDeJeu.SANSFIN)
+                    enCours = parts[3].trim();
+                else {
+                    enCours = "false";
+                }
+                if (!Boolean.parseBoolean(enCours) == niveau_en_cours) continue;
+                System.out.println("score : " + score + " date : " + date + " en cours : " + enCours + " mode de jeu : " + mode_de_jeu);
+                donneeScore.nom_joueur = nom_joueur;
+                donneeScore.score = score;
+                donneeScore.date = date;
+                donneeScore.niveau_en_cours = enCours;
+                scores.add(donneeScore);
+            }
+            bufferedReader.close();
+            for (DonneesScore donneesScore : scores) {
+                System.out.println("score : " + donneesScore.score + " date : " + donneesScore.date + " en cours : " + donneesScore.niveau_en_cours + " mode de jeu : " + mode_de_jeu);
+            }
+            return scores;
         }
-        bufferedReader.close();
-        for (DonneesScore donneesScore : scores) {
-            System.out.println("score : " + donneesScore.score + " date : " + donneesScore.date + " en cours : " + donneesScore.niveau_en_cours + " mode de jeu : " + mode_de_jeu);
-        }
-        return scores;
+        return null;
     }
 
     /**
@@ -553,14 +557,28 @@ public class Sauvegarder {
          * Le niveau en cours
          */
         public String niveau_en_cours;
+        /**
+         * Le nom du joueur
+         */
+        public String nom_joueur;
 
         /**
          * Constructeur
          *
-         * @param score le score
+         * @param n le nom de la personne.
+         * @param s le score de la personne.
+         * @param d la date du score fait.
          */
-        public DonneesScore(String score) {
-            this.score = score;
+        public DonneesScore(String n,String s, String d) {
+            this.nom_joueur = n;
+            this.score = s;
+            this.date = d;
+            this.niveau_en_cours = "";
+        }
+
+        public DonneesScore(){
+            this.nom_joueur = "";
+            this.score = "";
             this.date = "";
             this.niveau_en_cours = "";
         }
@@ -602,6 +620,13 @@ public class Sauvegarder {
             return Boolean.getBoolean(niveau_en_cours);
         }
 
-
+        /**
+         * Récupère le nom du joueur
+         *
+         * @return le nom du joueur
+         */
+        public String getNomJoueur() {
+            return nom_joueur;
+        }
     }
 }
